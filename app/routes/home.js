3eb5133ac62f97125a1571a2c06426e6a3c4d950
Henrique Borges <henriqueborges@Henriques-MacBook-Air.local>
2018-04-08 12:12:30 -0300

    changes

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,8 +1,10 @@
 module.exports = (application) => {
-    application.post("/api/analisar", (req, res) => {
+    console.log("application.app.controllers fora =", application.app);
+    application.post("/api/analisar", function (req, res){
         application.app.controllers.home.analisar(application, req, res);
     });
-    application.get("/", (req, res) => {
+    application.get("/", function (req, res){
+        console.log("application.app.controllers dentro =", application.app.controllers);
         application.app.controllers.home.render(application, req, res);
     });
 }
